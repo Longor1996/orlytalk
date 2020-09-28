f885a472304c48007044e65f762debef2ce0f921
Longor1996 <lalongok@gmail.com>
2020-09-28 19:06:43 +0200

    Make use of environment variables.
    Note: The port can't really be changed atm due to also being hardcoded in `index.ts`.

--- orly-server/src/main.rs
+++ orly-server/src/main.rs
@@ -13,17 +13,19 @@ use websocket::*;
 #[tokio::main]
 async fn main() {
     println!("Hello, world!");
+    dotenv::dotenv().ok();
     
     let current_exe = std::env::current_exe().expect("Executable Location");
     let working_dir = current_exe.parent().expect("Working Directory");
     println!("Working Directory: {:?}", working_dir);
     
-    let conn_path = working_dir.join("db.sqlite");
-    println!("Database File: {:?}", conn_path);
+    let db_file_name = std::env::var("ORLYTALK_SQLITE_FILE").unwrap_or_else(|_e| "db.sqlite".to_string());
+    let db_file_path = working_dir.join(db_file_name);
+    println!("Database File: {:?}", db_file_path);
     
-    let conn = rusqlite::Connection::open(conn_path).expect("Failed to start SQLite!");
+    let db_conn = rusqlite::Connection::open(db_file_path).expect("Failed to start SQLite!");
     
-    conn.execute("
+    db_conn.execute("
         CREATE TABLE IF NOT EXISTS users (
             id INTEGER NOT NULL UNIQUE PRIMARY KEY AUTOINCREMENT,
             uuid BLOB NOT NULL UNIQUE,
@@ -32,7 +34,10 @@ async fn main() {
     ", rusqlite::params![]).expect("SQLite Statement");
     
     // Clear out the cache...
-    conn.flush_prepared_statement_cache();
+    db_conn.flush_prepared_statement_cache();
+    
+    let db_conn = Arc::new(tokio::sync::Mutex::from(db_conn));
+    let db_conn = warp::any().map(move || db_conn.clone());
     
     let clients_map = Arc::new(DashMap::new());
     let clients_ref = warp::any().map(move || clients_map.clone());
@@ -42,7 +47,8 @@ async fn main() {
         .and(warp::query::<UserConnectionRequest>())
         .and(warp::ws())
         .and(clients_ref)
-        .map(|ucr: UserConnectionRequest, ws: warp::ws::Ws, users| {
+        .and(db_conn)
+        .map(|ucr: UserConnectionRequest, ws: warp::ws::Ws, users, _db_conn| {
             ws.on_upgrade(move |socket| client_connected(socket, ucr, users))
     });
     
@@ -74,13 +80,13 @@ async fn main() {
     
     let serve = warp::serve(routes);
     
-    let ip = [0, 0, 0, 0];
-    let port = 6991;
+    let host: std::net::IpAddr = std::env::var("ORLYTALK_HOST").unwrap_or_else(|_e| "0.0.0.0".to_string()).parse().expect("Valid host");
+    let port: u16              = std::env::var("ORLYTALK_PORT").unwrap_or_else(|_e| "6991".to_string()).parse().expect("Valid port number");
     
-    println!("Socket-IP: {:?}", ip);
+    println!("Socket-IP: {:?}", host);
     println!("Socket-Port: {}", port);
     
-    let addr = (ip, port);
+    let addr = (host, port);
     
     // Run forever!
     serve.run(addr).await;
